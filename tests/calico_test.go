1188649c803763742b84d78b4ab3b4c68de8d5dd
Kai Ehrhardt <kai.ehrhardt@sva.de>
2023-11-07 17:04:45 +0100

    fix(calico): fix semantic version input
    for further information check https://github.com/kairos-io/community-bundles/pull/56#discussion_r1384483279
    
    Signed-off-by: Kai Ehrhardt <kai.ehrhardt@sva.de>

--- tests/calico_test.go
+++ tests/calico_test.go
@@ -26,7 +26,7 @@ var _ = Describe("calico test", Label("calico"), func() {
 		Expect(content).To(ContainSubstring("version: \"3.25.0\""))
 	})
 
-	It("Specifiy version for calico", func() {
+	It("Specifiy major version for calico", func() {
 		err := os.WriteFile("/oem/foo.yaml", []byte(`#cloud-config
 calico:
  version: 1`), 0655)
@@ -37,6 +37,19 @@ calico:
 		content := string(dat)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(content).To(ContainSubstring("version: \"1\""))
+	})
+
+	It("Specifiy full version for calico", func() {
+		err := os.WriteFile("/oem/foo.yaml", []byte(`#cloud-config
+calico:
+ version: 1.2.3`), 0655)
+		Expect(err).ToNot(HaveOccurred())
+
+		runBundle()
+		dat, err := os.ReadFile(filepath.Join("/var/lib/rancher/k3s/server/manifests", "calico.yaml"))
+		content := string(dat)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(content).To(ContainSubstring("version: \"1.2.3\""))
 	})
 
 	It("Deploy calico with default values", func() {
