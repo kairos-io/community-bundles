a0108a9a65c27b7b6d2aa5c215b8c5e98b808089
Ludea <ludealol.olol@gmail.com>
2024-03-12 10:15:58 +0100

    argocd bundle
    Signed-off-by: Ludea <ludealol.olol@gmail.com>

---
+++ argocd/run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -ex
+
+K3S_MANIFEST_DIR=${K3S_MANIFEST_DIR:-/var/lib/rancher/k3s/server/manifests/}
+
+
+getConfig() {
+    local key=$1
+    _value=$(kairos-agent config get "${key} | @json" | tr -d '\n')
+    # Remove the quotes wrapping the value.
+    _value=${_value:1:${#_value}-2}
+    if [ "${_value}" != "null" ]; then
+     echo "${_value}"
+    fi 
+    echo   
+}
+
+VALUES="{}"
+VERSION="2.10.2"
+
+templ() {
+    local file="$3"
+    local value=$2
+    local sentinel=$1
+    sed -i "s/@${sentinel}@/${value}/g" "${file}"
+}
+
+readConfig() {
+    _values=$(getConfig argocd.values)
+    if [ "$_values" != "" ]; then
+        VALUES=$_values
+    fi
+    _version=$(getConfig argocd.version)
+    if [ "$_version" != "" ]; then
+        VERSION=$_version
+    fi
+}
+
+mkdir -p "${K3S_MANIFEST_DIR}"
+
+readConfig
+
+# Copy manifests, and template them
+for FILE in assets/*; do 
+  templ "VALUES" "${VALUES}" "${FILE}"
+  templ "VERSION" "${VERSION}" "${FILE}"
+done;
+
+cp -rf assets/* "${K3S_MANIFEST_DIR}"
